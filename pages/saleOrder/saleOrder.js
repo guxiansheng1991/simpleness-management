29e755d420481be24aee1622d3c8704804a1820b
xiansheng shen <2943887177@qq.com>
2019-04-05 19:50:45 +0800

    出库单和入库单

---
+++ pages/saleOrder/saleOrder.js
@@ -0,0 +1,22 @@
+const api = require('../../utils/api.js');
+const util = require('../../utils/util.js');
+const constant = require('../../utils/constant.js');
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  }
+});
